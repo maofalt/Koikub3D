ef5569d700c14cf6ef00a756accbd657ec902aad
Olivier MARTIN <olimarti@paul-f4Br6s6.clusters.42paris.fr>
2023-10-07 23:40:19 +0200

    🐛 fix(draw_utils.h): change draw_segment function signature to accept a const pointer to a const segment to improve code readability and prevent accidental modification of the segment 🐛 fix(draw_line.c): update draw_segment function implementation to use the const pointer to a const segment parameter instead of accessing the segment directly to improve code safety and prevent accidental modification of the segment

--- sources/draw_utils/draw_line.c
+++ sources/draw_utils/draw_line.c
@@ -6,25 +6,29 @@
 /*   By: olimarti <olimarti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/22 01:19:00 by olimarti          #+#    #+#             */
-/*   Updated: 2023/10/07 19:22:33 by olimarti         ###   ########.fr       */
+/*   Updated: 2023/10/07 19:47:36 by olimarti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "draw_utils.h"
 
-void	draw_segment(t_img_data *img, t_segment_d segment, int color)
+void	draw_segment(
+			t_img_data *img,
+			t_segment_d const *const segment,
+			int color
+			)
 {
 	t_point2d	delta;
 	t_point2d	cursor;
 	double		step;
 
-	delta.vec = segment.point_b.vec - segment.point_a.vec;
+	delta.vec = segment->point_b.vec - segment->point_a.vec;
 	if (fabs(delta.x) >= fabs(delta.y))
 		step = fabs(delta.x);
 	else
 		step = fabs(delta.y);
 	delta.vec = delta.vec / step;
-	cursor.vec = segment.point_a.vec;
+	cursor.vec = segment->point_a.vec;
 	while (step > 0)
 	{
 		img_pix_put(img, cursor.x, cursor.y, color);

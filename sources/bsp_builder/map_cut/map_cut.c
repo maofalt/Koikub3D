4e7034eff0433b9ef24c5c41a9897a3265125592
olimarmite <titolivier@gmx.fr>
2023-10-11 02:54:40 +0200

    refacto: simplify map_cut

--- sources/bsp_builder/map_cut/map_cut.c
+++ sources/bsp_builder/map_cut/map_cut.c
@@ -6,7 +6,7 @@
 /*   By: olimarti <olimarti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/11 01:04:00 by olimarti          #+#    #+#             */
-/*   Updated: 2023/10/11 01:48:06 by olimarti         ###   ########.fr       */
+/*   Updated: 2023/10/11 02:53:29 by olimarti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,37 +27,11 @@ typedef struct s_space_cut_context{
 	int				err;
 }	t_space_cut_context;
 
-static int	process_cut_segment_side_b(t_space_cut_context *context)
-{
-	t_segment_d	*segment_node;
 
-	segment_node = (t_segment_d *)context->segment_node->content;
-	if (context->point_a_side == 0)
-	{
-		if (context->point_b_side < 0)
-		{
-			*(segment_node) = (t_segment_d){context->segment_cpy.point_b,
-				context->intersection};
-			lst_move_node(context->left, &context->segment_node);
-		}
-		else if (context->point_b_side > 0)
-		{
-			segment_node->point_a = context->intersection;
-			lst_move_node(context->right, &context->segment_node);
-		}
-		return (0);
-	}
-	if (context->point_b_side < 0)
-		return (add_segment_to_lst(context->left, (t_segment_d)
-				{context->segment_cpy.point_b, context->intersection}));
-	if (context->point_b_side > 0)
-		return (add_segment_to_lst(context->right, (t_segment_d)
-				{context->intersection, context->segment_cpy.point_b}));
-	return (0);
-}
-
-static int	process_cut_segment(t_space_cut_context *context)
+static int	_cut_segment(t_space_cut_context *context)
 {
+	int	err;
+
 	context->intersection
 		= find_intersection(*context->separator, context->segment_cpy);
 	if (context->point_a_side < 0)
@@ -72,11 +46,16 @@ static int	process_cut_segment(t_space_cut_context *context)
 		{context->intersection, context->segment_cpy.point_a};
 		lst_move_node(context->right, &context->segment_node);
 	}
-	process_cut_segment_side_b(context);
-	return (0);
+	if (context->point_b_side < 0)
+		err = add_segment_to_lst(context->left, (t_segment_d)
+			{context->segment_cpy.point_b, context->intersection});
+	else
+		err = add_segment_to_lst(context->right, (t_segment_d)
+			{context->intersection, context->segment_cpy.point_b});
+	return (err);
 }
 
-static void	process_no_segment_intersection(t_space_cut_context *context)
+static void	_no_segment_intersection(t_space_cut_context *context)
 {
 	if (context->point_a_side < 0 || context->point_b_side < 0)
 		lst_move_node(context->left, &context->segment_node);
@@ -84,7 +63,7 @@ static void	process_no_segment_intersection(t_space_cut_context *context)
 		lst_move_node(context->right, &context->segment_node);
 }
 
-static t_space_cut_context	process_context_init(
+static t_space_cut_context	_context_init(
 	t_list **segments,
 	t_segment_d *separator,
 	t_list **left,
@@ -100,7 +79,6 @@ static t_space_cut_context	process_context_init(
 	context.separator = separator;
 	context.left = left;
 	context.right = right;
-	context.err = 0;
 	return (context);
 }
 
@@ -112,8 +90,8 @@ int	cut_space(
 {
 	t_space_cut_context	context;
 
-	context = process_context_init(segments, separator, left, right);
-	while (context.segment_node != NULL && !context.err)
+	context = _context_init(segments, separator, left, right);
+	while (context.segment_node != NULL)
 	{
 		context.segment_cpy = *(t_segment_d *)context.segment_node->content;
 		context.point_a_side = point_space_partitioning(separator,
@@ -125,10 +103,13 @@ int	cut_space(
 			lst_move_node(segments, &context.segment_node);
 			continue ;
 		}
-		else if (context.point_a_side * context.point_b_side <= 0)
-			context.err = process_cut_segment(&context);
+		else if (context.point_a_side * context.point_b_side < 0)
+		{
+			if (_cut_segment(&context))
+				return (1);
+		}
 		else
-			process_no_segment_intersection(&context);
+			_no_segment_intersection(&context);
 	}
-	return (context.err);
+	return (0);
 }

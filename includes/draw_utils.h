5dba71e03049e8d7e5d68d02d86848d934ddc853
motero home <motero@student.42.fr>
2023-10-14 15:00:53 +0200

    canvas structures and  lint

--- includes/draw_utils.h
+++ includes/draw_utils.h
@@ -16,6 +16,7 @@
 # include "mlx.h"
 # include "structures.h"
 # include "colors.h"
+# include "matrix.h"
 
 /*############################################################################*/
 /*                              GEOMETRY STRUTURE                             */
@@ -79,79 +80,93 @@ typedef struct s_shape_params {
 /*                              CANVAS STRUCTURES                             */
 /*############################################################################*/
 
-typedef struct s_dirty_rect {
-    int x, y, width, height;
-} t_dirty_rect;
+typedef enum e_canvas_type
+{
+	MAP,
+	UI,
+	FINAL
+}	t_canvas_type;
+
+typedef struct s_dirty_rect
+{
+	t_v2i	pos;
+	t_v2i	size;
+}	t_dirty_rect;
 
 typedef struct s_canvas {
-    t_color *pixels;
-    int width, height;
-    t_matrix3x3 transformation_matrix;
-    t_dirty_rect *dirty_rects;
-    int dirty_rect_count;
-    t_segment_d *segments; // This will be specific to the map_canvas.
-} t_canvas;
+	t_color			*pixels;
+	t_v2i			size;
+	t_matrix3x3		transformation_matrix;
+	t_dirty_rect	*dirty_rects;
+	int				dirty_rect_count;
+	t_segment_d		*segments;
+	t_canvas_type	type;
+}	t_canvas;
 
 /*############################################################################*/
 /*                              CANVAS INITIALIZATION                         */
 /*############################################################################*/
 
-t_canvas *initialize_canvas(int width, int height);
-void free_canvas(t_canvas *canvas);
+t_canvas	*initialize_canvas(t_v2i size);
+void		free_canvas(t_canvas *canvas);
 
 /*############################################################################*/
 /*                              MAP CANVAS OPERATIONS                         */
 /*############################################################################*/
 
-void add_dirty_rect(t_canvas *canvas, int x, int y, int width, int height);
-void add_segment_to_map(t_canvas *canvas, t_segment_d segment);
-void draw_line_on_map(t_canvas *canvas, t_point2d start, t_point2d end, t_color color);
-void erase_line_from_map(t_canvas *canvas, t_segment_d segment);
-void apply_transformation_to_map(t_canvas *canvas, t_matrix3x3 transform);
+int			add_dirty_rect(t_canvas *canvas, t_v2i coord, t_v2i size);
+int			add_segment_to_map(t_canvas *canvas, t_segment_d segment);
+int			draw_line_on_map(t_canvas *canvas,
+				t_point2d start,
+				t_point2d end,
+				t_color color);
+int			erase_line_from_map(t_canvas *canvas, t_segment_d segment);
+int			apply_transformation_to_map(t_canvas *canvas,
+				t_matrix3x3 transform);
 
 /*############################################################################*/
 /*                              UI CANVAS OPERATIONS                          */
 /*############################################################################*/
 
-void draw_UI_elements(t_canvas *ui_canvas);
+int			draw_UI_elements(t_canvas *ui_canvas);
 
 /*############################################################################*/
 /*                              FUSING CANVASES                               */
 /*############################################################################*/
 
-void fuse_canvases(t_canvas *final_canvas, t_canvas *map_canvas, t_canvas *ui_canvas);
+int			fuse_canvases(t_canvas **array_of_canvases);
 
 /*############################################################################*/
 /*                              MLX CONVERSION                                */
 /*############################################################################*/
 
-void *canvas_to_mlx_image(t_canvas *final_canvas);  // Returns the type MLX uses for images
+void		*canvas_to_mlx_image(t_canvas *final_canvas);
 
 /*############################################################################*/
 /*                              DRAW FUNCTIONS                                */
 /*############################################################################*/
 
-void	img_pix_put(t_img_data *img, int x, int y, int color);
-void	draw_segment(
-			t_img_data *img,
-			t_segment_d const *const segment,
-			int color
-			);
-void	draw_one_line(
-			t_img_data *img,
-			t_line_params const *const segment,
-			t_color color
-			);
+void		img_pix_put(t_img_data *img, int x, int y, int color);
+void		draw_segment(
+				t_img_data *img,
+				t_segment_d const *const segment,
+				int color
+				);
+void		draw_one_line(
+				t_img_data *img,
+				t_line_params const *const segment,
+				t_color color
+				);
 
 /*############################################################################*/
 /*                              DRAW SHAPES                                */
 /*############################################################################*/
-int		draw_line(t_shape_params *params);
-int		draw_rectangle(t_shape_params *params);
-void	draw_circle_points(t_img_data *img,
-			t_point2i center,
-			t_point2i point,
-			t_color color);
-int		draw_circle(t_shape_params *params);
+int			draw_line(t_shape_params *params);
+int			draw_rectangle(t_shape_params *params);
+void		draw_circle_points(t_img_data *img,
+				t_point2i center,
+				t_point2i point,
+				t_color color);
+int			draw_circle(t_shape_params *params);
 
 #endif

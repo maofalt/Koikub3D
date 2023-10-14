d97cd71741bb1a55f0999bce0bcd959765ef3e0f
olimarmite <titolivier@gmx.fr>
2023-10-14 18:07:25 +0200

    add basic separator vizualisation

--- sources/bsp_builder/bsp.c
+++ sources/bsp_builder/bsp.c
@@ -6,7 +6,7 @@
 /*   By: olimarti <olimarti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/08 00:15:03 by olimarti          #+#    #+#             */
-/*   Updated: 2023/10/14 06:31:17 by olimarti         ###   ########.fr       */
+/*   Updated: 2023/10/14 18:05:59 by olimarti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -72,6 +72,8 @@ typedef struct s_bsp_tree_node_data
 	t_sector_data	sector_data;
 }	t_bsp_tree_node_data;
 
+
+
 // int	cut_space(
 // 	t_list **segments,
 // 	t_segment_d *separator,
@@ -85,7 +87,6 @@ int construct_bsp(t_list **segments, t_tree_node **tree, t_cub *data)
 	t_list					*right;
 	t_bsp_tree_node_data	*node_data;
 	t_tree_node				*node;
-	t_segment_d	scaled_segment;
 
 	if (separator_node == NULL)
 		return (0);
@@ -103,15 +104,10 @@ int construct_bsp(t_list **segments, t_tree_node **tree, t_cub *data)
 	node->right = NULL;
 	node->data = node_data;
 	*tree = node;
-
-	scaled_segment = *(t_segment_d *)separator_node->content;
-	scaled_segment.point_a.vec *= 10;
-	scaled_segment.point_b.vec *= 10;
-	draw_segment(&data->screen, &scaled_segment,
-		0xFFFFFFFF);
+	draw_map_segments(data, *segments);
 	mlx_put_image_to_window(data->mlx_ptr, data->win_ptr,
 		data->screen.mlx_img, 0, 0);
-	usleep(1000000);
+	usleep(100000);
 	mlx_do_sync(data->mlx_ptr);
 	if (construct_bsp(&left, &node->left, data))
 		return (1); //todo free

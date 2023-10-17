650e60a0c57673932121c84ded43c70b27aa7a08
olimarmite <titolivier@gmx.fr>
2023-10-17 05:20:04 +0200

    Optimize ft_memset and ft_memcpy

--- libs/libft/sources/ft_memcpy.c
+++ libs/libft/sources/ft_memcpy.c
@@ -3,14 +3,29 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memcpy.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: motero <marvin@42.fr>                      +#+  +:+       +#+        */
+/*   By: olimarti <olimarti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/06 14:55:11 by motero            #+#    #+#             */
-/*   Updated: 2022/05/16 17:02:03 by motero           ###   ########.fr       */
+/*   Updated: 2023/10/17 04:27:42 by olimarti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+#ifdef __x86_64__
+
+void	*ft_memcpy(void *dest __attribute__((unused)),
+				const void *src __attribute__((unused)),
+				size_t n __attribute__((unused)))
+{
+	asm volatile (
+		"cld;"
+		"mov %rdx, %rcx;"
+		"rep movsb"
+	);
+	return (dest);
+}
+#else
 
 void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
@@ -19,7 +34,24 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 
 	d = dest;
 	s = src;
-	while (n-- && (d || s))
+	while (n && (((long)d % sizeof(long) != 0)
+			|| ((long)s % sizeof(long) != 0)))
+	{
 		*d++ = *s++;
+		n--;
+	}
+	while (n >= sizeof(long))
+	{
+		*(long *)d = *(const long *)s;
+		d += sizeof(long);
+		s += sizeof(long);
+		n -= sizeof(long);
+	}
+	while (n)
+	{
+		*d++ = *s++;
+		n--;
+	}
 	return (dest);
 }
+#endif
